8f90053622dc40283b91098ce7d046599b72e8ce
maddox05 <maddoxschmidlkofer@gmail.com>
2025-12-22 20:36:48 +0000

    fixed frontend

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 window.CONFIG = {
-  SIGNAL_URL: 'ws://localhost:8080',
+  SIGNAL_URL: window.location.hostname?.includes('localhost') ? 'ws://localhost:8080' : 'wss://api.maddoxcloud.com',
   SUPABASE_URL: "https://ztpnrydpwwdflohcomqe.supabase.co",
   SUPABASE_PUBLISHABLE_DEFAULT_KEY: "sb_publishable_Q4cyEpySuQTxHkpSxscrKw_64vJYF0H"
 
