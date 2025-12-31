d1c38c3c1ba9d5e2e9eff3d94c280a6ee8399509
maddox05 <maddoxschmidlkofer@gmail.com>
2025-12-31 14:08:39 -0500

    make init load a lil faster

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,10 +1,12 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from "./home/Home";
-import Pricing from "./pricing/Pricing";
-import Queue from "./queue/Queue";
-import InGame from "./in_game/InGame";
+
+const Home = lazy(() => import("./home/Home"));
+const Pricing = lazy(() => import("./pricing/Pricing"));
+const Queue = lazy(() => import("./queue/Queue"));
+const InGame = lazy(() => import("./in_game/InGame"));
 
 function AppLayout() {
   const location = useLocation();
@@ -13,12 +15,14 @@ function AppLayout() {
   return (
     <>
       {!hideNavFooter && <Navbar />}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/queue/:appId" element={<Queue />} />
-        <Route path="/app/:appId" element={<InGame />} />
-      </Routes>
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/queue/:appId" element={<Queue />} />
+          <Route path="/app/:appId" element={<InGame />} />
+        </Routes>
+      </Suspense>
       {!hideNavFooter && <Footer />}
     </>
   );
