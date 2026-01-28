e0986fafbf2629136b131d7869a0f62c5bc60561
maddox05 <maddoxschmidlkofer@gmail.com>
2026-01-28 14:15:48 +0000

    fixc

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base:
+    process.env.GITHUB_PAGES === "true"
+      ? `/${process.env.GITHUB_REPOSITORY!.split("/")[1]}/`
+      : "/",
+});
